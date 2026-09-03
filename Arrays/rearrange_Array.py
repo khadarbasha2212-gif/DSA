757eee034b63e4dd416e8f2f764d349d1363a2fb
khadarbasha <khadarbasha2212@gmail.com>
2026-09-03 18:01:27 +0530

    re arrange array

--- Arrays/rearrange_Array.py
+++ Arrays/rearrange_Array.py
@@ -1,38 +1,18 @@
 class Solution:
-
     def rearrangeArray(self, nums):
-
-        # Create an empty array to store the result
         result = [0] * len(nums)
 
-        # Index for positive numbers
-        positive_index = 0
+        pos = 0
+        neg = 1
 
-        # Index for negative numbers
-        negative_index = 1
-
-        # Go through every number in nums
         for num in nums:
-
-            # If the number is positive
             if num > 0:
-
-                # Put it at the next positive position
-                result[positive_index] = num
-
-                # Move to the next positive position
-                positive_index += 2
-
-            # If the number is negative
+                result[pos] = num
+                pos += 2
             else:
+                result[neg] = num
+                neg += 2
 
-                # Put it at the next negative position
-                result[negative_index] = num
-
-                # Move to the next negative position
-                negative_index += 2
-
-        # Return the rearranged array
         return result
 nums = [3, 1, -2, -5, 2, -4]
 
